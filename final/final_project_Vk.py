c94acea98247caabc895c27c60f49a9fea9add9d
mayakorotkaya <maya1535@yandex.ru>
2018-06-16 23:06:45 +0300

    Update final_project_Vk.py

--- final/final_project_Vk.py
+++ final/final_project_Vk.py
@@ -151,10 +151,8 @@ def thanks():
     return redirect(url_for(''))
 
 if __name__ == '__main__':
-    import os
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+
 
 
 
